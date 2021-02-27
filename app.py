3cf52176e668508b9c750dc126217a7f6ce8e3f6
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-02-27 13:18:49 +0530

    added mongoDB & basic api routes

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+import csv
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/weedDB"
+mongo = PyMongo(app)
 
 
 @app.route('/')
 def hello():
     return "Hello This is Exam Meet Backend App!"
+
+@app.route("/api/login",methods=["POST"])
+def loginHere():
+    return
+
+@app.route("/api/signup",methods=["POST"])
+def signupHere():
+    return
 
 if __name__ == '__main__':
     app.run(debug=True)
